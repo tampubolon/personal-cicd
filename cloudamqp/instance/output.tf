1fc36f0ff38e671233428b5c82d86e1a70e769d6
tampubolon <tampubolonmartinus@gmail.com>
2024-02-07 10:40:43 +0700

    fix terraform files

--- cloudamqp/instance/output.tf
+++ cloudamqp/instance/output.tf
@@ -1,4 +1,9 @@
-output "vpc_info" {
-  value       = cloudamqp_instance.pintu_test
-  description = "RabbitMQ Instance Info"
+output "instance_id" {
+  value       = cloudamqp_instance.pintu_test.id
+  description = "RabbitMQ Instance ID"
+}
+
+output "instance_host" {
+  value       = cloudamqp_instance.pintu_test.host
+  description = "RabbitMQ Instance Host"
 }
