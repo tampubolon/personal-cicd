a23f05ff9e9ecc6d8682075b759f0ad2fbcb9e6f
tampubolon <tampubolonmartinus@gmail.com>
2024-02-07 10:32:19 +0700

    fix terraform files

---
+++ cloudamqp/instance/main.tf
@@ -0,0 +1,34 @@
+locals {
+  instance_name = "pintu-test"
+  environment   = "test"
+  owner         = "team-sre"
+  cloudamqp_tag = ["Terraform"]
+  vpc_id        = "196329"
+  region        = "amazon-web-services::ap-southeast-1"
+  rmq_plan      = "lemur"
+  rmq_version   = "3.10.5"
+
+  default_tags = {
+    Environment = local.environment
+    Owner       = local.owner
+    ManagedBy   = "terraform"
+    TFProject   = "github.com/tampubolon/pintu-infra/cloudamqp/instance"
+  }
+}
+
+data "aws_ssm_parameter" "api_key" {
+  name = "/iss/staging/cloudamqp/cloudamqp-key"
+}
+
+# Configure the CloudAMQP Provider
+provider "cloudamqp" {
+  apikey = data.aws_ssm_parameter.api_key.value
+}
+
+# Add instance to managed VPC
+resource "cloudamqp_instance" "pintu_test" {
+  name    = local.instance_name
+  plan    = local.rmq_plan
+  region  = local.region
+  tags    = ["terraform"]
+}
---
+++ cloudamqp/instance/output.tf
@@ -0,0 +1,4 @@
+output "vpc_info" {
+  value       = cloudamqp_instance.pintu_test
+  description = "RabbitMQ Instance Info"
+}
