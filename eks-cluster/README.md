2c4563151b77524b0414e3f5cba233aa16ea65e9
tampubolon <tampubolonmartinus@gmail.com>
2024-02-09 13:41:48 +0700

    provision EKS cluster

---
+++ eks-cluster/README.md
@@ -0,0 +1,3 @@
+# EKS Cluster
+This is terraform project to provision EKS Cluster
+This EKS cluster built on top of VPC created on Terraform project: https://github.com/tampubolon/pintu-infra/tree/main/eks-vpc
---
+++ eks-cluster/main.tf
@@ -0,0 +1,93 @@
+locals {
+  vpc_id          = "vpc-0ca2d1fea23bcd3a9"
+  public_subnets  = ["subnet-0c0f72c6359ad6085", "subnet-087efb745876fa202", "subnet-0fe65694cdabca319"]
+  private_subnets = ["subnet-0ba62fd249ca8dffe", "subnet-0bd942bd682d9b8c0", "subnet-0ae005bb4777fe7e1"]
+}
+
+# Create an EKS cluster
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = local.public_subnets
+  }
+
+  tags = {
+    Name = "martinus-eks-cluster"
+  }
+}
+
+# Create the IAM role for the EKS cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "eks.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach policies to the EKS cluster role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Create a worker node IAM role
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+# Create a security group for the EKS cluster
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "eks-cluster-sg"
+  description = "Security group for the EKS cluster"
+  vpc_id      = local.vpc_id
+}
+
+# Allow inbound and outbound traffic for the EKS cluster security group
+resource "aws_security_group_rule" "eks_cluster_ingress" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_cluster_sg.id
+}
+
+resource "aws_security_group_rule" "eks_cluster_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_cluster_sg.id
+}
+
+# Output the EKS cluster details
+output "eks_cluster_name" {
+  value = aws_eks_cluster.my_cluster.name
+}
